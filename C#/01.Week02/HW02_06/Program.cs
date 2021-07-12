79ac3248483383efac2e3660b2bef07203768f97
CHunYenc <yuhnoesty0402@hotmail.com.tw>
2021-07-13 01:18:12 +0800

    feat: Add C#

---
+++ C#/01.Week02/HW02_06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+/* 
+HW2-6: 請寫一個程式能讓使用者輸入五位學生的成績，
+然後再計算這五位學生成績相加後的總分，
+以及五位學生的平均成績，
+最後在畫面中顯示出這五位學生成績的相加總分及平均分數。
+*/
+
+namespace HW02_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double D1 = 0;
+            Double D2 = 0;
+            Double D3 = 0;
+            Double D4 = 0;
+            Double D5 = 0;
+
+            Console.Write("請輸入第 1 位學生的成績：");
+            D1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("請輸入第 2 位學生的成績：");
+            D2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("請輸入第 3 位學生的成績：");
+            D3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("請輸入第 4 位學生的成績：");
+            D4 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("請輸入第 5 位學生的成績：");
+            D5 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"五位學生成績的相加總分為：{D1 + D2 + D3 + D4 + D5}，平均分數為：{(D1+D2+D3+D4+D5)/5}");
+            
+        }
+    }
+}
---
+++ C#/01.Week02/HW02_10/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+/*
+HW2-10:請使用if或if-else指令撰寫一程式，
+讓使用者可由鍵盤輸入x、y座標值(可包含正負數及小數)，
+然後判斷使用者輸入的座標點位於那一個象限或是在座標軸上。
+舉例來說，若輸入的座標值為(3.0,-2.5)，
+輸出即為第四象限；若輸入的座標值為(4.5,0.0)，
+則輸出即為x軸。
+(註:需考慮4個象限、X軸、Y軸及原點;
+二座標數字在各象限的正負號為，第一象限++，第二象限-+，第三象限--，第四象限+-) 
+*/
+
+namespace HW02_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+
+            Console.WriteLine("請輸入你的 x 值：");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("請輸入你的 y 值：");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            if (x == 0 && y == 0)
+            {
+                Console.WriteLine($"您輸入的 x = {x}, y = {y} 在原點上.");
+            } else if ( x == 0 && y != 0)
+            {
+                Console.WriteLine($"您輸入的 x = {x}, y = {y} 在 y 軸線上.");
+            } else if ( x != 0 && y == 0)
+            {
+                Console.WriteLine($"您輸入的 x = {x}, y = {y} 在 x 軸線上.");
+            } else if ( x != 0 || y != 0)
+            {
+                if (x > 0)
+                {
+                    if (y > 0)
+                    {
+                        Console.WriteLine($"您輸入的 x = {x}, y = {y} 在 第一象限.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"您輸入的 x = {x}, y = {y} 在 第四象限.");
+                    }
+                }
+                else
+                {
+                    if (y > 0)
+                    {
+                        Console.WriteLine($"您輸入的 x = {x}, y = {y} 在 第二象限.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"您輸入的 x = {x}, y = {y} 在 第三象限.");
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine($"您輸入的 x = {x}, y = {y} 在好像有不是數值的.");
+            }
+        }
+    }
+}
